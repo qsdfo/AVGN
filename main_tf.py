a9a7032ef97a5b4872b2bd643ab8fd57eb1d71a8
Leo Crestel <crestel.leopold@gmail.com>
2020-03-28 08:40:43 +0100

    up

--- main_tf.py
+++ main_tf.py
@@ -39,6 +39,16 @@ def main():
     local_device_protos = device_lib.list_local_devices()
     print([x.name for x in local_device_protos if x.device_type == 'GPU'])
 
+    import tensorflow as tf
+    with tf.device('/gpu:0'):
+        a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+        b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+        c = tf.matmul(a, b)
+
+    with tf.Session() as sess:
+        print (sess.run(c))
+
+    import pdb; pdb.set_trace()
     ##########################################################################################
     # Data
     # Define data parameters
