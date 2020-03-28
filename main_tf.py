dc81e22879730370bd00e10febd429788423f501
Leo Crestel <crestel.leopold@gmail.com>
2020-03-28 08:46:01 +0100

    up

--- main_tf.py
+++ main_tf.py
@@ -28,8 +28,6 @@ load = f'{module_path}/data/models/GAIA_CAVI/2020-03-27_15-50-59/0_model.tfmod'
 
 
 def main():
-    import pdb
-    pdb.set_trace()
     ##########################################################################################
     # Environment
     gpus = [0]  # Here I set CUDA to only see one GPU
@@ -39,17 +37,10 @@ def main():
     if len(gpus) < 1:
         num_gpus = 1
     local_device_protos = device_lib.list_local_devices()
-    print([x.name for x in local_device_protos if x.device_type == 'GPU'])
+    print([x.name for x in local_device_protos if x.device_type in ['XLA_GPU', 'GPU']])
+    import pdb
+    pdb.set_trace()
 
-    with tf.device('/gpu:0'):
-        a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-        b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-        c = tf.matmul(a, b)
-
-    with tf.Session() as sess:
-        print (sess.run(c))
-
-    import pdb; pdb.set_trace()
     ##########################################################################################
     # Data
     # Define data parameters
